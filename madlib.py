4597ba2a3cf09c896dd945a7b18eecf1acfd6227
Cases18704 <sergeylankevich@gmail.com>
2018-11-05 02:35:12 +0700

    Stage completed.

---
+++ madlib.py
@@ -0,0 +1,26 @@
+name = input('Type in a silly name: ')
+word = input('Type in a silly word: ')
+verb = input('Type in a verb: ')
+noun = input('Type in a noun: ')
+body = input('Type in a body part (plural): ')
+f_name = input('Type in a female name: ')
+verbed = input('Type in a verb ending with "ed": ')
+noun_2 = input('Type in a noun: ')
+noun_3 = input('Type in a noun(plural): ')
+verb_2 = input('Type in a verb: ')
+noun_4 = input('Type in a noun: ')
+occupation = input('Type in an occupation: ')
+number = input('Type in a number: ')
+verb_5 = input('Type in a verb: ')
+word_2 = input('Type in a silly word: ')
+name_1 = input('Type in a silly name: ')
+
+print('Will you let me {} your {}? Ever since I have laid {} on {}, I have madly in love with her.'.format(verb, noun,
+                                                                                                           body,
+                                                                                                           f_name))
+print('I wish that she will be the {} of my {} and that someday we will {} happily ever after. '.format(verbed, noun_2,
+                                                                                                        noun_3))
+print('I have a {} as a {} that pays ${} each month. I promise to {} with kindness and respect.'.format(verb_2, noun_4,
+                                                                                                        occupation,
+                                                                                                        number, verb_5,
+                                                                                                        word_2, name_1))
